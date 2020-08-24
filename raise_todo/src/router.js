64b42c983e08107275e300332e910a58c780f762
Sken <k18056kk@aitech.ac.jp>
2020-08-24 20:19:44 +0900

    【FIX】todoリストに飛べるようにした

--- raise_todo/src/router.js
+++ raise_todo/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "./pages/Home.vue";
+import Todo from "./pages/Todo.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "./pages/About.vue"),
+  },
+  {
+    path: "/todo",
+    name: "Todo",
+    component: Todo,
   },
 ];
 
