43e3bbb8c2bda31aa6d19b912899880ea09c460e
Sken <k18056kk@aitech.ac.jp>
2020-08-24 17:40:00 +0900

    【FIX】初期ファイルをtemplateに置き換えた

---
+++ raise_todo/src/components/HomeIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "",
+  data: () => ({}),
+};
+</script>
