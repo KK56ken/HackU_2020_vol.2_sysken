f59a452db2958988a8584400e7ed3c670fe60b7e
wkympand0 <ari7ri2it1018@gmail.com>
2020-08-25 19:44:32 +0900

    育成画面にボタン追加

--- raise_todo/src/pages/Raising.vue
+++ raise_todo/src/pages/Raising.vue
@@ -3,10 +3,22 @@
     <v-container>
       <v-row class="text-center">
         <v-col cols="6" sm="6" md="4" lg="3">
+          
           <h1>育成</h1>
-          <v-divider></v-divider>
+          <v-divider>  
+          </v-divider>
+
+           
         </v-col>
       </v-row>
+           
+            <v-avatar color="#7C5736" size="120">
+             <span class="white--text headline">エサ </span> 
+            </v-avatar> 
+            <v-avatar color="#0094FF" size="78">
+             <span class="white--text headline caption">コレクション </span> 
+             
+            </v-avatar> 
     </v-container>
   </div>
 </template>
