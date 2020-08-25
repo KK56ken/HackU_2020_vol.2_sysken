88d1b3941bfb51cbb9229eed05e9966df4cfa41c
Sken <k18056kk@aitech.ac.jp>
2020-08-25 16:54:10 +0900

    【ADD】raisingページの追加

---
+++ raise_todo/src/pages/Raising.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="raising">
+    <v-container>
+      <v-row class="text-center">
+        <v-col cols="6" sm="6" md="4" lg="3">
+          <h1>育成</h1>
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
