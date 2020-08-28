47dea56f86464e1fe71405d50f541bbe0137f9db
Sken <k18056kk@aitech.ac.jp>
2020-08-28 23:05:31 +0900

    【FIX】サインインのpostメソッドを作った

--- raise_todo/src/pages/SignUp.vue
+++ raise_todo/src/pages/SignUp.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="SingUp">
-  
     <v-container>
-        <v-card width="400px" class="mx-auto mt-5">
-         <v-card-title>
-             <h1 class="display-1">登録</h1>
+      <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">登録</h1>
         </v-card-title>
         <v-card-text>
-         <v-form>
-          <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
-             <v-text-field v-bind:type="showPassword ? 'text' : 'password'" prepend-icon="mdi-lock" v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" label="パスワード" @click:append="showPassword = !showPassword" />
-              <v-card-actions>
-              <v-btn>登録</v-btn>
-             </v-card-actions>
-        </v-form>
+          <v-form>
+            <v-text-field
+              v-model="user_name"
+              prepend-icon="mdi-account-circle"
+              label="ユーザ名"
+            />
+            <v-text-field
+              v-model="password"
+              v-bind:type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              label="パスワード"
+              @click:append="showPassword = !showPassword"
+            />
+            <v-card-actions>
+              <v-btn v-on:click="signup()">登録</v-btn>
+            </v-card-actions>
+          </v-form>
         </v-card-text>
-        </v-card>
+      </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
 export default {
- 
-  
   data: () => ({
-    showPassword : false
-  })
+    user_name: "",
+    password: "",
+    showPassword: false,
+  }),
+  methods: {
+    signup() {
+      this.$store.dispatch("post_new_user", {
+        name: this.user_name,
+        password: this.password,
+      });
+      // if (this.$store.state.user.logined == false) {
+      //   setTimeout(this.enable, 1500);
+      // }
+    },
+  },
 };
-
 </script>
--- raise_todo/src/store.js
+++ raise_todo/src/store.js
@@ -7,11 +7,16 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     todos: [],
+    user: { name: "", password: "", token: "" },
   },
   mutations: {
     add_todo(state, todo) {
       // 状態を変更する
       state.todos.push(todo);
+    },
+    set_user(state, post_data) {
+      state.user.name = post_data.name;
+      state.user.password = post_data.password;
     },
   },
   actions: {
@@ -24,6 +29,24 @@ export default new Vuex.Store({
         })
         .then(() => {
           console.log("送れた");
+        })
+        .catch((err) => {
+          if (err.response) {
+            console.log("送れなかった");
+          }
+        });
+    },
+    post_new_user(context, user) {
+      axios
+        .post("http://127.0.0.1:3001/user/signup", {
+          user_name: user.name,
+          password: user.password,
+        })
+        .then((res) => {
+          console.log(res.data);
+          if (res.data) {
+            context.commit("set_user", res.data);
+          }
         })
         .catch((err) => {
           if (err.response) {
